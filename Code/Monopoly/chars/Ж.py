a8b02e9e7eee783e820f299c68a957284110d222
Immersive Programs <80697141+Immersive-programs@users.noreply.github.com>
2023-01-12 20:48:34 +0700

    Update Ж.py

--- Code/Monopoly/chars/Ж.py
+++ Code/Monopoly/chars/Ж.py
@@ -1,4 +1,10 @@
 class char:
     def draw(buf,x,y,index,col):
         zero = x+8*index
-        buf.text('E', zero, y, col)
+        buf.line(zero,y,zero+6,y+6,col)
+        buf.line(zero+6,y,zero,y+6,col)
+        buf.line(zero+3,y,zero+3,y+6,col)
+        buf.line(zero,y+1,zero+3,y+3,col)
+        buf.line(zero,y+5,zero+3,y+3,col)
+        buf.line(zero+6,y+1,zero+3,y+3,col)
+        buf.line(zero+6,y+5,zero+3,y+3,col)
